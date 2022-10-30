4ab1b78fc50a7c54e718433c1a34e1c29c0667a7
ycozco <ycozco@unsa.edu.pe>
2022-10-30 16:58:46 -0500

    upload

---
+++ lab04/sigusrl.c
@@ -0,0 +1,22 @@
+//sigusr1.c
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+sig_atomic_t sigusr1_contador = 0;
+
+void manejador(int nro_senal) {
+    ++sigusr1_contador;
+    printf("SIGUSR1 se dio %d veces\n", sigusr1_contador);
+    }
+int main() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &manejador;
+    sigaction(SIGUSR1, &sa, NULL);
+    //por el bucle infinito el programa debe ser
+    //abortado mediante SIGKILL o SIGTERM
+    while(1);
+    return 0;
+}
